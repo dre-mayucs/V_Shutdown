e79684cadfe627dd91a0255b8c532b1099c26060
mayu---cs <hato182@gmail.com>
2019-07-25 11:02:22 +0900

    First commit
    飽きた

---
+++ V_Shutdown/V_Shutdown/Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace V_Shutdown
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.Help;
+            this.textBox1.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textBox1.ForeColor = System.Drawing.Color.White;
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(309, 116);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "help\r\n\r\nShutdown now -OS Shutdown\r\nReboot now -OS Reboot\r\nSystem exit -Applicatio" + "n exit\r\n";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(334, 137);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "V_Shutdown";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ V_Shutdown/V_Shutdown/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Speech.Recognition;
+
+namespace V_Shutdown
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] Command = Properties.Resources.Commands.Split(',');
+            try
+            {
+                var Engine = new SpeechRecognitionEngine();
+                Engine.SetInputToDefaultAudioDevice();
+
+                var choices = new Choices(Command);
+                var GBuilder = new GrammarBuilder();
+                GBuilder.Append(choices);
+                var grammar = new Grammar(GBuilder);
+                Engine.LoadGrammar(grammar);
+
+                //エンジンモード
+                Engine.RecognizeAsync(RecognizeMode.Multiple);
+
+                //認識イベントハンドラ
+                Engine.SpeechRecognized += Engie_Recognized;
+            }
+            catch
+            {
+                MessageBox.Show("Engine Error" + Environment.NewLine + "マイクが有効になっていないかコマンドデータが破損しています");
+            }
+        }
+
+        private void Engie_Recognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            try
+            {
+                if (e.Result.Confidence >= 0.300000)
+                {
+                    if (e.Result.Text == "Shutdown now") { Shutdown(); }
+                    else if (e.Result.Text == "Reboot now") { reboot(); }
+                    else if (e.Result.Text == "System exit") { exit(); }
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Engine Error");
+                Application.Exit();
+            }
+        }
+
+        private void Shutdown()
+        {
+            DialogResult result = MessageBox.Show("シャットダウンしますか?", "Message", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+            if (DialogResult.OK == result)
+            {
+                var process = new ProcessStartInfo
+                {
+                    FileName = "shutdown.exe",
+                    Arguments = "-s -t 0",
+                    CreateNoWindow = true
+                };
+                Process.Start(process);
+                Application.Exit();
+            }
+        }
+
+        private void reboot()
+        {
+            DialogResult result = MessageBox.Show("再起動しますか?", "Message", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+            if (DialogResult.OK == result)
+            {
+                var process = new ProcessStartInfo
+                {
+                    FileName = "shutdown.exe",
+                    Arguments = "-r -t 0",
+                    CreateNoWindow = true
+                };
+                Process.Start(process);
+                Application.Exit();
+            }
+        }
+
+        private void exit()
+        {
+            DialogResult result = MessageBox.Show("終了しますか?", "Message", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+            if (DialogResult.OK == result) { Application.Exit(); }
+        }
+    }
+}
