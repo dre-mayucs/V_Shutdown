7a933ee7bb4ddc19f51cf67f28a1d041ab418b36
mayu___cs <53264288+mayu-cs@users.noreply.github.com>
2020-03-07 22:45:23 +0900

    Update(small)
    add lambda expression

--- V_Shutdown/V_Shutdown/Form1.cs
+++ V_Shutdown/V_Shutdown/Form1.cs
@@ -7,10 +7,7 @@ namespace V_Shutdown
 {
     public partial class Form1 : Form
     {
-        public Form1()
-        {
-            InitializeComponent();
-        }
+        public Form1() => InitializeComponent();
 
         private void Form1_Load(object sender, EventArgs e)
         {
